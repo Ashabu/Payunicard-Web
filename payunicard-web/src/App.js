81fbeb9f801a0638efdfdace4d2604df36ef42d4
Avtandil Shaburishvili <a.shaburishvili@gmail.com>
2021-04-07 21:25:15 +0400

    initial folder setup

---
+++ payunicard-web/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import React from 'react';
+
+function App() {
+
+  return (
+    <div className="App">
+      <span>hello world</span>
+    </div>
+  );
+}
+
+export default App;

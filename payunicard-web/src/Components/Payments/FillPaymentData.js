4ec8ecd70cd4eacee3169313855e16e469769e76
Avtandil Shaburishvili <a.shaburishvili@gmail.com>
2021-05-28 10:02:28 +0400

    fill payment data component

--- payunicard-web/src/Components/Payments/FillPaymentData.js
+++ payunicard-web/src/Components/Payments/FillPaymentData.js
@@ -1,4 +1,4 @@
-import React, {useContext, useState} from 'react';
+import React, {useContext, useState, Fragment} from 'react';
 import './payment.scss';
 import { Context } from '../../Context/AppContext';
 import { Button, Icon, Input, Select, SelectList } from '../UI/UiComponents';
@@ -14,9 +14,15 @@ const  FillPaymentData = (props) => {
 
     const [ selectedAccount, setSelectedAccount] = useState({});
     const [ abonentCode, setAbonentCode ] = useState('');
-    const [ costumerDebtData, setCostumerDebtData ] = useState([]);
+    const [ debt, setDebt ] = useState('');
+    const [ costumer, setCostumer ] = useState('');
+    const [ costumerAddress,  setCostumerAddress] = useState('');
+    const [ errorMessage, setErrorMessage ] = useState('')
+
 
     const { merchantdata } = props;
+
+    console.log(merchantdata)
 
 
     const checkCostumerDebt = () => {
@@ -29,8 +35,15 @@ const  FillPaymentData = (props) => {
         }
 
         Presentation.checkCostumerDebt(checkDetails).then(res => {
-            if(res.ok) {
-                setCostumerDebtData(res.Data.Structures);
+            debugger
+            if(res.data.Ok) {
+                let tempDebtData = res.data.Data.Structures
+                setDebt(tempDebtData[0].Value);
+                setCostumer(tempDebtData[1].Value);
+                setCostumerAddress(tempDebtData[2].Value);
+                
+            } else {
+                 setErrorMessage(res.data.Errors[0].DisplayText)
             }
         })
 
@@ -41,7 +54,8 @@ const  FillPaymentData = (props) => {
     
     
     return (
-        <div style={{width: 570, height: '100%', border: '1px solid black', boxSizing: 'border-box', padding: 20}}>
+         <div style={{width: 570, height: '100%', border: '1px solid black', boxSizing: 'border-box', padding: 20}}>
+           
             <div>
                 <p>Please Choose a Card</p>
                 <Select
@@ -57,16 +71,41 @@ const  FillPaymentData = (props) => {
                         </SelectList>)} 
                     />
             </div>
-            <div style={{display: 'flex', justifyContent: 'space-between'}}>
+            <div style={{display: 'flex', justifyContent: 'space-between', alignItems: 'center', marginTop: 30}}>
                 <div style={{width: 300}}>
-
+                    <div style = {{display: 'flex'}}>
+                        <img style = {{width: 70}} src ={merchantdata.merchantImgUrl} alt = 'merchant logo' />
+                        <p>{merchantdata.merchantName}</p>
+                    </div>           
                 </div>
-                <div style = {{display: 'flex'}}>
+                <div style = {{display: 'flex', flexFlow: 'column'}}>
                     <Input value = {abonentCode} onInput = {(e) => setAbonentCode(e.target.value)} />
                     {abonentCode !== ''? <Button clicked = {checkCostumerDebt}>შემომწმება</Button> : null}
                 </div>
             </div>
-        </div>
+            <div className = 'payment-info'>
+                <div>{errorMessage}</div>
+                <div className = 'd-flex'>
+                    <p>დავალიანება:</p>
+                    <p>{debt}</p>
+                </div>
+                <div className = 'd-flex'>
+                    <p>აბონენტი:</p>
+                    <p>{costumer}</p>
+                </div>
+                <div className = 'd-flex'>
+                    <p>მისამართი:</p>
+                    <p>{costumerAddress}</p>
+                </div>
+                <div className = 'al-it-center'>
+                    <p>თანხის ოდენობა:</p>
+                    <Input/>
+                </div>
+                
+
+
+            </div>
+        </div> 
     )
 }
 

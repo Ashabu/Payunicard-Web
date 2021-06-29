17145eaf9ea49c605c152f565f87229ff34a4a13
Avtandil Shaburishvili <a.shaburishvili@gmail.com>
2021-06-29 10:33:15 +0400

    accounts and cards

--- payunicard-web/src/Components/Myproducts/AccountCard.js
+++ payunicard-web/src/Components/Myproducts/AccountCard.js
@@ -1,7 +1,9 @@
 import React, {useState} from 'react';
 import './accountCard.scss';
 import PropTypes from 'prop-types'
-import { formatNumber, setLogoByAccountType } from '../../Services/CommonFunctions'
+import { formatNumber, setLogoByAccountType } from '../../Services/CommonFunctions';
+import ClickableBulltes from '../HOC/ClickableBulltes';
+
 
 const AccountCard = (props) => {
     const { accountName, accountNumber, accountTypeName, availableInEUR, availableInGBP, availableInGEL, availableInRUB, availableInTRY, availableInUSD , cards, currencies, imageUrl, type, mAskedCard , customerPaketId } = props.account;
@@ -19,6 +21,22 @@ const AccountCard = (props) => {
                 ))}
             </div>
         )
+    }
+    
+
+    const getAccountColorByType  = (id) => {
+        let accountClass = '';
+        if(id === 2) {
+            accountClass = 'Upera'
+        } else if (id === 3) {
+            accountClass = 'UniPlus'
+        } else if (id === 4) {
+            accountClass = 'UniUltra'
+        } else {
+            accountClass = 'Wallet'
+        }
+
+        return accountClass;
     }
 
     
@@ -80,19 +98,26 @@ const AccountCard = (props) => {
 
 
     return (
-        <div className = 'multicard'>
-        <div className = 'AccountCard' onClick = { clickCard }>
-            
-            <span className = 'ac-name'>{ accountTypeName } { mAskedCard }</span>
-            <span className = 'ac-balance'> { formatNumber(availableBalance) } { BalanceCcy || balanceCcy }</span>
-            <img className = 'click-icon' src = '../../Assets/Images/three_dot.png' alt= 'copy-icon'/>
-            {type !== 7 && <AccountCurrencies />} 
-            <div className = 'ac-number'>
-                <span> { accountNumber }</span>
-                <img src = '../../Assets/Images/copy-icon.png' alt= 'copy-icon'/>
+        <div className = {'ac-container ' +  getAccountColorByType(customerPaketId)}>
+           {cards.length > 1?  <div className = 'multicard'>
+                <div className = 'triangle'></div>
+            </div> : null}
+            <div className = 'ac-card' onClick = { clickCard }>
+                {cards.length > 0? <div className = 'triangle'></div> : null}
+                <span className = 'ac-name'>{ accountTypeName } { mAskedCard }</span>
+                <span className = 'ac-balance'> { formatNumber(availableBalance) } { BalanceCcy || balanceCcy }</span>
+                {cards.length > 1? <span className = 'ac-cardnumber'>{ cards.length }</span>: null }
+                <img className = 'click-icon' src = '../../Assets/Images/three_dot.png' alt= 'copy-icon'/>
+                {type !== 7 && <AccountCurrencies />} 
+                <div className = 'ac-number'>
+                    <span> { accountNumber }</span>
+                    <img src = '../../Assets/Images/copy-icon.png' alt= 'copy-icon'/>
+                </div>
+                <img className = 'ac-logo' src = { setLogoByAccountType(type) } alt = 'logo' />            
             </div>
-            <img className = 'ac-logo' src = { setLogoByAccountType(type) } alt = 'logo' />            
-        </div>
+            <div> {cards.length > 1? cards.map((c, i) => (
+                <ClickableBulltes key = { i } bullets = 'ac-bullets'/>
+            )): null }</div>
         </div>
     )
 }
